4eb3b73521b242d0351262665fdd7e83680cb057
Dominc Carolan <dominiccarolan@gmail.com>
2019-10-07 20:56:56 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -13,7 +13,10 @@ def display_card_total(card)
   puts "Your cards add up to #{card}"
 end
 
-
+def prompt_user
+  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
+end
 
 #####################################################
 # get every test to pass before coding runner below #
