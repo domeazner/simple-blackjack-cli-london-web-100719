401a219761a0fd217d8a25db74d336cfa800e9ed
Dominc Carolan <dominiccarolan@gmail.com>
2019-10-07 20:56:06 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -3,7 +3,10 @@ def welcome
   puts "Welcome to the Blackjack Table"
 end
 
-
+def deal_card(card = 1..11)
+  # code #deal_card here
+  rand(card)
+end
 
 #####################################################
 # get every test to pass before coding runner below #
